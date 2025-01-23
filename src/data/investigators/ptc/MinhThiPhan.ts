223c020dda581e709b3dfbf5c6ad74b1fbd4b95c
Vladimir Yazykov <neizerth@gmail.com>
2025-01-23 11:39:03 +0300

    added minh info

--- src/data/investigators/ptc/MinhThiPhan.ts
+++ src/data/investigators/ptc/MinhThiPhan.ts
@@ -1,3 +1,23 @@
-export const MinhThiPhan = {
-  
+import type { Investigator } from "@/types/game";
+
+export const MinhThiPhan: Investigator = {
+  code: '03002',
+  image: {
+    width: 1445,
+    height: 1138,
+    media: [
+      {
+        width: 673,
+        height: 1033,
+        left: 203,
+        top: 80
+      },
+      {
+        width: 860,
+        height: 1058,
+        left: 170,
+        top: 80
+      }
+    ]
+  }
 }
