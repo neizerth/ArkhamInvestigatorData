d5a9f7b15226709b5d8db3f911abb1ed079a98ec
Vladimir Yazykov <neizerth@gmail.com>
2025-01-24 13:57:14 +0300

    tfa prepare

---
+++ src/data/investigators/tfa/index.ts
@@ -0,0 +1,15 @@
+import { CalvinWright } from "./CalvinWright";
+import { FatherMateo } from "./FatherMateo";
+import { FinnEdwards } from "./FinnEdwards";
+import { LeoAnderson } from "./LeoAnderson";
+import { UrsulaDowns } from "./UrsulaDowns";
+import { YithianBody } from "./YithianBody";
+
+export default [
+  LeoAnderson,
+  UrsulaDowns,
+  FinnEdwards,
+  FatherMateo,
+  CalvinWright,
+  YithianBody
+];
