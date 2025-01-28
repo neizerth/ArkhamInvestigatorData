9670291d015803bd2fa878634c120a2cf6a33597
Vladimir Yazykov <neizerth@gmail.com>
2025-01-28 13:45:43 +0300

    fhv dummy data

---
+++ src/data/campaigns/fhv/index.ts
@@ -0,0 +1,16 @@
+import { AlessandraZorzi } from "./AlessandraZorzi";
+import { HankSamson } from "./HankSamson";
+import { KateWinthrop } from "./KateWinthrop";
+import { KohakuNarukami } from "./KohakuNarukami";
+import { ShatteredSelf } from "./ShatteredSelf";
+import { WilsonRichards } from "./WilsonRichards";
+
+
+export default [
+  WilsonRichards,
+  KateWinthrop,
+  AlessandraZorzi,
+  KohakuNarukami,
+  HankSamson,
+  ShatteredSelf
+]
