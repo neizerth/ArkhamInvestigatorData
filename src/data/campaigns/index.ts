ec1829427ff70e8744c7d1e99d7acee148f66adc
Vladimir Yazykov <neizerth@gmail.com>
2025-01-29 13:06:26 +0300

    updated index

--- src/data/campaigns/index.ts
+++ src/data/campaigns/index.ts
@@ -3,11 +3,21 @@ import dwl from './dwl';
 import ptc from './ptc';
 import tfa from './tfa';
 import tcu from './tcu';
+import tde from './tde';
+import tic from './tic';
+import eoe from './eoe';
+import tsk from './tsk';
+import fhv from './fhv';
 
 export default [
   ...core,
   ...dwl,
   ...ptc,
   ...tfa,
-  ...tcu
+  ...tcu,
+  ...tde,
+  ...tic,
+  ...eoe,
+  ...tsk,
+  ...fhv
 ]
