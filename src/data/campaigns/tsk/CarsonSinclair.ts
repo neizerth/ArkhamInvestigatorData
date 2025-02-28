339a04c86dc9f9ea6dd8e84523e447030c417c6d
Vladimir Yazykov <neizerth@gmail.com>
2025-02-28 20:50:56 +0300

    updated carson

--- src/data/campaigns/tsk/CarsonSinclair.ts
+++ src/data/campaigns/tsk/CarsonSinclair.ts
@@ -4,13 +4,13 @@ export const CarsonSinclair: Investigator = {
   code: '09001',
   additionalAction: true,
   image: {
-    width: 2476,
-    height: 3216,
+    width: 1760,
+    height: 2119,
     face: {
-      width: 588,
-      height: 667,
-      left: 744,
-      top: 290
+      width: 318,
+      height: 340,
+      left: 663,
+      top: 686
     },
     media: [
       {
