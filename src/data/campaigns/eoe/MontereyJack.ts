bc822ef14a56f2005edfa9aa42b0a79196f058bf
Vladimir Yazykov <neizerth@gmail.com>
2025-03-16 14:11:13 +0300

    updated monty

--- src/data/campaigns/eoe/MontereyJack.ts
+++ src/data/campaigns/eoe/MontereyJack.ts
@@ -3,23 +3,14 @@ import type { Investigator } from "@/model";
 export const MontereyJack: Investigator = {
   code: '08007',
   image: {
-    width: 2112,
-    height: 1790,
+    width: 2717,
+    height: 2430,
     face: {
       width: 288,
-      height: 315,
-      left: 622,
-      top: 539
-    },
-    media: [
-      {
-        width: 989,
-        height: 1520,
-        left: 160,
-        top: 270,
-        primary: true
-      }
-    ]
+      height: 351,
+      left: 1232,
+      top: 1143
+    }
   },
   variants: [
     {
