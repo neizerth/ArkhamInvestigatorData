edd8dffe2d25f7f7052fad3d566c9c1b1d5264a5
Vladimir Yazykov <neizerth@gmail.com>
2025-03-19 14:21:17 +0300

    updated daniela

--- src/data/campaigns/eoe/DanielaReyes.ts
+++ src/data/campaigns/eoe/DanielaReyes.ts
@@ -3,28 +3,13 @@ import type { Investigator } from "@/model";
 export const DanielaReyes: Investigator = {
   code: '08001',
   image: {
-    width: 2691,
-    height: 3509,
+    width: 2332,
+    height: 2500,
     face: {
-      width: 518,
-      height: 647,
-      left: 950,
-      top: 839
-    },
-    media: [
-      {
-        width: 1393,
-        height: 2141,
-        left: 231,
-        top: 202,
-        primary: true
-      },
-      {
-        width: 1700,
-        height: 2822,
-        left: 120,
-        top: 202
-      }
-    ]
+      width: 318,
+      height: 381,
+      left: 1012,
+      top: 855
+    }
   }
 }
