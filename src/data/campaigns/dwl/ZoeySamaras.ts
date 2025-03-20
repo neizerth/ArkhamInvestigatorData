3c8b39fe5927d0f625dddeab6ecee4e682215a80
Vladimir Yazykov <neizerth@gmail.com>
2025-03-20 19:31:08 +0300

    updated zoey / preston

--- src/data/campaigns/dwl/ZoeySamaras.ts
+++ src/data/campaigns/dwl/ZoeySamaras.ts
@@ -3,29 +3,14 @@ import type { Investigator } from "@/model";
 export const ZoeySamaras: Investigator = {
   code: '02001',
   image: {
-    width: 1382,
-    height: 1800,
+    width: 1817,
+    height: 2240,
     face: {
-      width: 200,
-      height: 272,
-      left: 781,
-      top: 676
-    },
-    media: [
-      {
-        width: 750,
-        height: 1153,
-        left: 494,
-        top: 73,
-        primary: true
-      },
-      {
-        width: 814,
-        height: 1213,
-        left: 430,
-        top: 50
-      }
-    ]
+      width: 218,
+      height: 285,
+      left: 768,
+      top: 1095
+    }
   },
   variants: [
     {
--- src/data/campaigns/tcu/PrestonFairmont.ts
+++ src/data/campaigns/tcu/PrestonFairmont.ts
@@ -3,28 +3,13 @@ import type { Investigator } from "@/model";
 export const PrestonFairmont: Investigator = {
   code: '05003',
   image: {
-    width: 2457,
-    height: 3224,
+    width: 1791,
+    height: 3120,
     face: {
-      width: 417,
-      height: 545,
-      left: 1161,
-      top: 565
-    },
-    media: [
-      {
-        width: 1543,
-        height: 2371,
-        left: 523,
-        top: 400,
-        primary: true
-      },
-      {
-        width: 1934,
-        height: 2771,
-        left: 523,
-        top: 400
-      }
-    ]
+      width: 316,
+      height: 373,
+      left: 860,
+      top: 1187
+    }
   }
 }
