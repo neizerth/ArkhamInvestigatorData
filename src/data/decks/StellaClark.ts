3a228e15203ac0cbabfbc720ba14a5d545d55ed7
Vladimir Yazykov <neizerth@gmail.com>
2025-03-23 17:38:05 +0300

    updated stella

--- src/data/decks/StellaClark.ts
+++ src/data/decks/StellaClark.ts
@@ -4,28 +4,13 @@ export const StellaClark: Investigator = {
   code: '60501',
   additionalAction: true,
   image: {
-    width: 3032,
-    height: 2160,
+    width: 3127,
+    height: 2500,
     face: {
-      width: 302,
-      height: 377,
-      left: 758,
-      top: 553
-    },
-    media: [
-      {
-        width: 1014,
-        height: 1557,
-        left: 388,
-        top: 261,
-        primary: true
-      },
-      {
-        width: 1720,
-        height: 1900,
-        left: 0,
-        top: 260
-      }
-    ],
+      width: 336,
+      height: 393,
+      left: 1316,
+      top: 1053
+    }
   }
 }
