c6e12b4107940b082ae89fa3cae53ab2c74c0bca
Vladimir Yazykov <neizerth@gmail.com>
2025-03-31 13:11:08 +0300

    updated sefina

--- src/data/campaigns/ptc/SefinaRousseau.ts
+++ src/data/campaigns/ptc/SefinaRousseau.ts
@@ -3,28 +3,13 @@ import type { Investigator } from "@/model";
 export const SefinaRousseau: Investigator ={
   code: '03003',
   image: {
-    width: 2048,
-    height: 2048,
+    width: 3308,
+    height: 2968,
     face: {
-      width: 223,
-      height: 377,
-      left: 646,
-      top: 556
-    },
-    media: [
-      {
-        width: 936,
-        height: 1436,
-        left: 262,
-        top: 466,
-        primary: true
-      },
-      {
-        width: 936,
-        height: 1582,
-        left: 262,
-        top: 466
-      }
-    ]
+      width: 258,
+      height: 343,
+      left: 1734,
+      top: 1397
+    }
   }
 }
