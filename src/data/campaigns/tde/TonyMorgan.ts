ee81805e7b78c43f09ff12329778152c895c5f7b
Vladimir Yazykov <neizerth@mail.ru>
2025-04-06 00:54:51 +0300

    Update TonyMorgan.ts

--- src/data/campaigns/tde/TonyMorgan.ts
+++ src/data/campaigns/tde/TonyMorgan.ts
@@ -9,23 +9,8 @@ export const TonyMorgan: Investigator = {
     face: {
       width: 338,
       height: 328,
-      left: 738,
+      left: 738 - 200,
       top: 1145
     },
-    media: [
-      {
-        width: 941,
-        height: 1444,
-        left: 33,
-        top: 134,
-        primary: true
-      },
-      {
-        width: 941,
-        height: 1574,
-        left: 33,
-        top: 134
-      }
-    ]
   }
 }
