8f973bb0d558976f874032108f7f3a5d7c860a20
Vladimir Yazykov <neizerth@gmail.com>
2025-04-09 17:41:24 +0300

    updated jim

--- src/data/campaigns/tde/LukeRobinson.ts
+++ src/data/campaigns/tde/LukeRobinson.ts
@@ -1,7 +1,7 @@
 import type { Investigator } from "@/model";
 
 export const LukeRobinson: Investigator = {
-  code: '06004',
+  code: "06004",
   image: {
     width: 2415,
     height: 2370,
@@ -9,22 +9,7 @@ export const LukeRobinson: Investigator = {
       width: 230,
       height: 307,
       left: 1116,
-      top: 1023
+      top: 1023,
     },
-    media: [
-      {
-        width: 750,
-        height: 1161,
-        left: 441,
-        top: 240,
-        primary: true
-      },
-      {
-        width: 1320,
-        height: 1360,
-        left: 150,
-        top: 240
-      }
-    ]
-  }
-}
+  },
+};
