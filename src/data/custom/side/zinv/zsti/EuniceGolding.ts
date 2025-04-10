f8c34ce9ece9f2e9cc1509e8d94e883d3bf4c678
Vladimir Yazykov <neizerth@gmail.com>
2025-04-10 08:22:21 +0300

    added eunice ability

--- src/data/custom/side/zinv/zsti/EuniceGolding.ts
+++ src/data/custom/side/zinv/zsti/EuniceGolding.ts
@@ -1,7 +1,7 @@
 import type { Investigator } from "@/model";
 
 export const EuniceGolding: Investigator = {
-  code: 'zsti_00013',
+  code: "zsti_00013",
   image: {
     width: 1786,
     height: 2382,
@@ -9,22 +9,14 @@ export const EuniceGolding: Investigator = {
       width: 264,
       height: 289,
       left: 416,
-      top: 1157
+      top: 1157,
     },
-    media: [
-      {
-        width: 744,
-        height: 1142,
-        left: 61,
-        top: 942,
-        primary: true
-      },
-      {
-        width: 1228,
-        height: 1440,
-        left: 0,
-        top: 942
-      }
-    ]
-  }
-}
+  },
+  abilities: [
+    {
+      id: "reaction",
+      type: "reaction",
+      limitPer: "turn",
+    },
+  ],
+};
