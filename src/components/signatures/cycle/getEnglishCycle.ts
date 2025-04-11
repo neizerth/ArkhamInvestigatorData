c05d1788c9dcdea8b083db6164661678f048b3ac
Vladimir Yazykov <neizerth@gmail.com>
2025-04-11 18:57:21 +0300

    updated types

--- src/components/signatures/cycle/getEnglishCycle.ts
+++ src/components/signatures/cycle/getEnglishCycle.ts
@@ -1,21 +1,12 @@
-import type { ArkhamCardsPack } from "@/api/arkhamCards";
-import type {
-  InvestigatorSignatureCycle,
-  InvestigatorSignaturePack,
-} from "@/model";
+import type { ArkhamCardsCycle, ArkhamCardsPack } from "@/api/arkhamCards";
+import type { InvestigatorSignatureCycle } from "@/model";
 import { getCycleIcon } from "./getCycleIcon";
-
-type Options = {
-  code: string;
-  real_name: string;
-  position: number;
-};
 
 export const getEnglishCycle = ({
   code,
   real_name,
   position,
-}: ArkhamCardsPack): InvestigatorSignatureCycle => ({
+}: ArkhamCardsCycle): InvestigatorSignatureCycle => ({
   locale: "en",
   code: code,
   name: real_name,
