96aa3b611546e41251327b6b0d4c31afe9c3df58
Vladimir Yazykov <neizerth@gmail.com>
2025-04-14 13:54:18 +0300

    updated yuki

--- src/data/custom/campaigns/zgoo/YukiYagami.ts
+++ src/data/custom/campaigns/zgoo/YukiYagami.ts
@@ -1,30 +1,15 @@
 import type { Investigator } from "@/model";
 
 export const YukiYagami: Investigator = {
-	code: "zgoo_00001",
-	image: {
-		width: 6000,
-		height: 2592,
-		face: {
-			width: 386,
-			height: 451,
-			left: 2210,
-			top: 797,
-		},
-		media: [
-			{
-				width: 565,
-				height: 853,
-				left: 1488,
-				top: 481,
-				primary: true,
-			},
-			{
-				width: 6000,
-				height: 853,
-				left: 0,
-				top: 481,
-			},
-		],
-	},
+  code: "zgoo_00001",
+  image: {
+    width: 2845,
+    height: 3000,
+    face: {
+      width: 207,
+      height: 328,
+      left: 1368,
+      top: 1336,
+    },
+  },
 };
