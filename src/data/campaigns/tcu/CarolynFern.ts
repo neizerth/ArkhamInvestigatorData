1c81102c9acf0817b1aae8b26bcb741cddf053e8
Vladimir Yazykov <neizerth@gmail.com>
2025-04-16 11:17:46 +0300

    updated carolyn

--- src/data/campaigns/tcu/CarolynFern.ts
+++ src/data/campaigns/tcu/CarolynFern.ts
@@ -1,32 +1,49 @@
 import type { Investigator } from "@/model";
+import { image } from "../tdc/AgathaCrane";
 
 export const CarolynFern: Investigator = {
-	code: "05001",
-	image: {
-		width: 1770,
-		height: 2130,
-		face: {
-			width: 201,
-			height: 296,
-			left: 648,
-			top: 730,
-		},
-	},
-	variants: [
-		{
-			code: "98010",
-			type: "book",
-			name: "To Fight the Black Wind",
-			image: {
-				width: 3516,
-				height: 4000,
-				face: {
-					width: 228,
-					height: 333,
-					left: 1640,
-					top: 1779,
-				},
-			},
-		},
-	],
+  code: "05001",
+  image: {
+    width: 1770,
+    height: 2130,
+    face: {
+      width: 201,
+      height: 296,
+      left: 648,
+      top: 730,
+    },
+  },
+  variants: [
+    {
+      code: "98010",
+      type: "book",
+      name: "To Fight the Black Wind",
+      image: {
+        width: 3516,
+        height: 4000,
+        face: {
+          width: 228,
+          height: 333,
+          left: 1640,
+          top: 1779,
+        },
+      },
+    },
+  ],
+  skins: [
+    {
+      id: "05001_rational-thought",
+      name: "Rational Thought",
+      image: {
+        width: 8640,
+        height: 6270,
+        face: {
+          width: 192,
+          height: 293,
+          left: 4160,
+          top: 2933,
+        },
+      },
+    },
+  ],
 };
