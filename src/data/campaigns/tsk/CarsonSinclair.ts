835bb5bc090be8376b5e626f15bcbe83b012dac6
Vladimir Yazykov <neizerth@gmail.com>
2025-05-03 09:10:09 +0300

    faet: updated carson action

--- src/data/campaigns/tsk/CarsonSinclair.ts
+++ src/data/campaigns/tsk/CarsonSinclair.ts
@@ -1,25 +1,25 @@
 import type { Investigator } from "@/model";
 
 export const CarsonSinclair: Investigator = {
-	code: "09001",
-	additionalAction: true,
-	image: {
-		width: 1760,
-		height: 2119,
-		face: {
-			width: 318,
-			height: 340,
-			left: 663,
-			top: 686,
-		},
-	},
-	abilities: [
-		{
-			id: "give-action",
-			icon: "plus",
-			type: "action",
-			limitPer: "round",
-			perInvestigator: true,
-		},
-	],
+  code: "09001",
+  additionalAction: true,
+  image: {
+    width: 1760,
+    height: 2119,
+    face: {
+      width: 318,
+      height: 340,
+      left: 663,
+      top: 686,
+    },
+  },
+  abilities: [
+    {
+      id: "give-action",
+      icon: "plus",
+      type: "special-action",
+      limitPer: "round",
+      perInvestigator: true,
+    },
+  ],
 };
