8ff2f0ef096f0c86ef060316cbc4161af18a8943
Vladimir Yazykov <neizerth@gmail.com>
2025-05-04 14:31:12 +0300

    feat: 7 skins

--- src/data/campaigns/fhv/KohakuNarukami.ts
+++ src/data/campaigns/fhv/KohakuNarukami.ts
@@ -1,23 +1,39 @@
 import type { Investigator } from "@/model";
 
 export const KohakuNarukami: Investigator = {
-	code: "10012",
-	image: {
-		width: 2500,
-		height: 2109,
-		face: {
-			width: 238,
-			height: 321,
-			left: 1110,
-			top: 933,
-		},
-	},
-	abilities: [
-		{
-			id: "bless-curse",
-			type: "action",
-			icon: "seal",
-			limitPer: "round",
-		},
-	],
+  code: "10012",
+  image: {
+    width: 2500,
+    height: 2109,
+    face: {
+      width: 238,
+      height: 321,
+      left: 1110,
+      top: 933,
+    },
+  },
+  abilities: [
+    {
+      id: "bless-curse",
+      type: "action",
+      icon: "seal",
+      limitPer: "round",
+    },
+  ],
+  skins: [
+    {
+      id: "10012_ethereal-weaving",
+      name: "Ethereal Weaving",
+      image: {
+        width: 1896,
+        height: 1714,
+        face: {
+          width: 204,
+          height: 238,
+          left: 819,
+          top: 659,
+        },
+      },
+    },
+  ],
 };
--- src/data/campaigns/tic/AmandaSharpe.ts
+++ src/data/campaigns/tic/AmandaSharpe.ts
@@ -1,15 +1,31 @@
 import type { Investigator } from "@/model";
 
 export const AmandaSharpe: Investigator = {
-	code: "07002",
-	image: {
-		width: 2354,
-		height: 2500,
-		face: {
-			width: 380,
-			height: 457,
-			left: 1096,
-			top: 1023,
-		},
-	},
+  code: "07002",
+  image: {
+    width: 2354,
+    height: 2500,
+    face: {
+      width: 380,
+      height: 457,
+      left: 1096,
+      top: 1023,
+    },
+  },
+  skins: [
+    {
+      id: "07002_encyclopedia",
+      name: "Encyclopedia",
+      image: {
+        width: 2354,
+        height: 2500,
+        face: {
+          width: 380,
+          height: 457,
+          left: 1096,
+          top: 1023,
+        },
+      },
+    },
+  ],
 };
