295b4482e253fd83aba473156e97172305c410e8
Vladimir Yazykov <neizerth@gmail.com>
2025-05-04 19:16:17 +0300

    fix: wrong locale

--- src/components/signatures/getSignatureTranslations.ts
+++ src/components/signatures/getSignatureTranslations.ts
@@ -4,44 +4,54 @@ import { getCycleTranslation } from "./cycle";
 import { getSignatureBase } from "./getSignatureBase";
 import { getPackTranslation } from "./pack";
 
+const textAttributes = ["traits", "name", "subname", "text"] as const;
+
 export const getSignatureTranslations = (data: ArkhamCardsInvestigator[]) => {
-	const translations = new Map<string, InvestigatorSignature[]>();
+  const translations = new Map<string, InvestigatorSignature[]>();
 
-	for (const card of data) {
-		const base = getSignatureBase(card);
-		if (!base) {
-			continue;
-		}
+  for (const card of data) {
+    const base = getSignatureBase(card);
 
-		for (const cardTranslation of card.translations) {
-			const { locale } = cardTranslation;
-			if (!translations.has(locale)) {
-				translations.set(locale, []);
-			}
+    if (!base) {
+      continue;
+    }
 
-			const pack = getPackTranslation({
-				item: card.pack,
-				locale,
-			});
+    for (const cardTranslation of card.translations) {
+      const { locale } = cardTranslation;
+      if (!translations.has(locale)) {
+        translations.set(locale, []);
+      }
 
-			const cycle = getCycleTranslation({
-				item: card.pack.cycle,
-				locale,
-			});
+      const pack = getPackTranslation({
+        item: card.pack,
+        locale,
+      });
 
-			const icon = base.type === "taboo" ? "taboo" : pack.icon;
-			const translatedCard: InvestigatorSignature = {
-				...base,
-				...cardTranslation,
-				official: base.official,
-				pack,
-				cycle,
-				icon,
-			};
+      const cycle = getCycleTranslation({
+        item: card.pack.cycle,
+        locale,
+      });
 
-			translations.get(locale).push(translatedCard);
-		}
-	}
+      const translated = textAttributes.every(
+        (prop) => card[`real_${prop}`] !== cardTranslation[prop]
+      );
 
-	return translations;
+      const realLocale = translated ? locale : "en";
+
+      const icon = base.type === "taboo" ? "taboo" : pack.icon;
+      const translatedCard: InvestigatorSignature = {
+        ...base,
+        ...cardTranslation,
+        locale: realLocale,
+        official: base.official,
+        pack,
+        cycle,
+        icon,
+      };
+
+      translations.get(locale).push(translatedCard);
+    }
+  }
+
+  return translations;
 };
