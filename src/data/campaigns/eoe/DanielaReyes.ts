0abdafd390c5c5e02fddd929ce4099ce13dbc4b5
Vladimir Yazykov <neizerth@gmail.com>
2025-05-04 08:21:08 +0300

    fixed carolyn / added daniela

--- src/data/campaigns/eoe/DanielaReyes.ts
+++ src/data/campaigns/eoe/DanielaReyes.ts
@@ -1,15 +1,31 @@
 import type { Investigator } from "@/model";
 
 export const DanielaReyes: Investigator = {
-	code: "08001",
-	image: {
-		width: 2332,
-		height: 2500,
-		face: {
-			width: 318,
-			height: 381,
-			left: 1012,
-			top: 855,
-		},
-	},
+  code: "08001",
+  image: {
+    width: 2332,
+    height: 2500,
+    face: {
+      width: 318,
+      height: 381,
+      left: 1012,
+      top: 855,
+    },
+  },
+  skins: [
+    {
+      id: "08001_not-without-a-fight",
+      name: "Not without a fight",
+      image: {
+        width: 1762,
+        height: 1646,
+        face: {
+          width: 113,
+          height: 143,
+          left: 824,
+          top: 751,
+        },
+      },
+    },
+  ],
 };
