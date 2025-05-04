6bd9994a1eb1574d6b36b65d9a01a4e2aa02eb5a
Vladimir Yazykov <neizerth@gmail.com>
2025-05-04 08:40:04 +0300

    added akachi skin

--- src/data/campaigns/ptc/AkachiOnyele.ts
+++ src/data/campaigns/ptc/AkachiOnyele.ts
@@ -1,17 +1,31 @@
 import type { Investigator } from "@/model";
 
-const shiftX = 300;
-
 export const AkachiOnyele: Investigator = {
-	code: "03004",
-	image: {
-		width: 3992,
-		height: 4196,
-		face: {
-			width: 438,
-			height: 631,
-			left: 1290 + shiftX,
-			top: 1467,
-		},
-	},
+  code: "03004",
+  image: {
+    width: 3992,
+    height: 4196,
+    face: {
+      width: 438,
+      height: 631,
+      left: 1290 + 300,
+      top: 1467,
+    },
+  },
+  skins: [
+    {
+      id: "03004_coc",
+      name: "Call of Cthulhu",
+      image: {
+        width: 5840,
+        height: 4256,
+        face: {
+          width: 351,
+          height: 415,
+          left: 2751,
+          top: 1730,
+        },
+      },
+    },
+  ],
 };
