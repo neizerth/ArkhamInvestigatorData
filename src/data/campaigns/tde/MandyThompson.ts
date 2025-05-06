c916754c6527af438ea9be71195beeb58c3df7ba
Vladimir Yazykov <neizerth@gmail.com>
2025-05-06 20:17:31 +0300

    feat: mandy skin

--- src/data/campaigns/tde/MandyThompson.ts
+++ src/data/campaigns/tde/MandyThompson.ts
@@ -1,22 +1,38 @@
 import type { Investigator } from "@/model";
 
 export const MandyThompson: Investigator = {
-	code: "06002",
-	image: {
-		width: 2465,
-		height: 2645,
-		face: {
-			width: 284,
-			height: 367,
-			left: 1176,
-			top: 1141,
-		},
-	},
-	abilities: [
-		{
-			id: "reaction",
-			type: "reaction",
-			limitPer: "round",
-		},
-	],
+  code: "06002",
+  image: {
+    width: 2465,
+    height: 2645,
+    face: {
+      width: 284,
+      height: 367,
+      left: 1176,
+      top: 1141,
+    },
+  },
+  abilities: [
+    {
+      id: "reaction",
+      type: "reaction",
+      limitPer: "round",
+    },
+  ],
+  skins: [
+    {
+      id: "06002_shocking-discovery",
+      name: "Shocking Discovery",
+      image: {
+        width: 3380,
+        height: 3180,
+        face: {
+          width: 308,
+          height: 397,
+          left: 1456,
+          top: 1375,
+        },
+      },
+    },
+  ],
 };
