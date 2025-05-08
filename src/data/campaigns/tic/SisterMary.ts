b82ec84ddf2f155fcff88514a7b4c02a369d46cc
Vladimir Yazykov <neizerth@gmail.com>
2025-05-08 13:03:49 +0300

    feat: sister mary

--- src/data/campaigns/tic/SisterMary.ts
+++ src/data/campaigns/tic/SisterMary.ts
@@ -1,23 +1,39 @@
 import type { Investigator } from "@/model";
 
 export const SisterMary: Investigator = {
-	code: "07001",
-	image: {
-		width: 1980,
-		height: 2220,
-		face: {
-			width: 282,
-			height: 339,
-			left: 932,
-			top: 939,
-		},
-	},
-	abilities: [
-		{
-			id: "reaction",
-			type: "reaction",
-			// works when turn end
-			limitPer: "turn",
-		},
-	],
+  code: "07001",
+  image: {
+    width: 1980,
+    height: 2220,
+    face: {
+      width: 282,
+      height: 339,
+      left: 932,
+      top: 939,
+    },
+  },
+  abilities: [
+    {
+      id: "reaction",
+      type: "reaction",
+      // works when turn end
+      limitPer: "turn",
+    },
+  ],
+  skins: [
+    {
+      id: "07001_guardian-angel",
+      name: "Guardian Angel",
+      image: {
+        width: 3283,
+        height: 3746,
+        face: {
+          width: 159,
+          height: 214,
+          left: 1641,
+          top: 1757,
+        },
+      },
+    },
+  ],
 };
