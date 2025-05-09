a941c8c13c59a2c7eb1ad67550c1b21a1f32d502
Vladimir Yazykov <neizerth@gmail.com>
2025-05-09 22:04:49 +0300

    feat: harvey skins

--- src/data/decks/HarveyWalters.ts
+++ src/data/decks/HarveyWalters.ts
@@ -1,37 +1,52 @@
 import type { Investigator } from "@/model";
 
 export const HarveyWalters: Investigator = {
-	code: "60201",
-	image: {
-		width: 3170,
-		height: 2991,
-		face: {
-			width: 372,
-			height: 532,
-			left: 1266,
-			top: 1208,
-		},
-		media: [
-			{
-				width: 995,
-				height: 1529,
-				left: 139,
-				top: 209,
-				primary: true,
-			},
-			{
-				width: 1200,
-				height: 1396,
-				left: 0,
-				top: 340,
-			},
-		],
-	},
-	abilities: [
-		{
-			id: "reaction",
-			type: "reaction",
-			limitPer: "round",
-		},
-	],
+  code: "60201",
+  image: {
+    width: 3170,
+    height: 2991,
+    face: {
+      width: 372,
+      height: 532,
+      left: 1266,
+      top: 1208,
+    },
+  },
+  abilities: [
+    {
+      id: "reaction",
+      type: "reaction",
+      limitPer: "round",
+    },
+  ],
+  skins: [
+    {
+      id: "60201_occult-invocation",
+      name: "Occult Invocation",
+      image: {
+        width: 4170,
+        height: 3982,
+        face: {
+          width: 297,
+          height: 352,
+          left: 1986,
+          top: 1580,
+        },
+      },
+    },
+    {
+      id: "60201_thrice-damned-curiosity",
+      name: "Thrice-damned Curiosity",
+      image: {
+        width: 2500,
+        height: 2226,
+        face: {
+          width: 286,
+          height: 324,
+          left: 1176,
+          top: 819,
+        },
+      },
+    },
+  ],
 };
