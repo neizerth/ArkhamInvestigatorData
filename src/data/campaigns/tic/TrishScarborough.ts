1131d110aea8186b90bd216803ee1826785a0c93
Vladimir Yazykov <neizerth@gmail.com>
2025-05-10 14:03:39 +0300

    feat: trish skin

--- src/data/campaigns/tic/TrishScarborough.ts
+++ src/data/campaigns/tic/TrishScarborough.ts
@@ -1,36 +1,52 @@
 import type { Investigator } from "@/model";
 
 export const TrishScarborough: Investigator = {
-	code: "07003",
-	image: {
-		width: 1640,
-		height: 1416,
-		face: {
-			width: 242,
-			height: 317,
-			left: 646,
-			top: 607,
-		},
-	},
-	abilities: [
-		{
-			id: "reaction",
-			type: "reaction",
-			limitPer: "round",
-		},
-	],
-	variants: [
-		{
-			type: "taboo",
-			code: "07003",
-			name: "Taboo",
-			abilities: [
-				{
-					id: "reaction",
-					type: "reaction",
-					limitPer: "round",
-				},
-			],
-		},
-	],
+  code: "07003",
+  image: {
+    width: 1640,
+    height: 1416,
+    face: {
+      width: 242,
+      height: 317,
+      left: 646,
+      top: 607,
+    },
+  },
+  abilities: [
+    {
+      id: "reaction",
+      type: "reaction",
+      limitPer: "round",
+    },
+  ],
+  variants: [
+    {
+      type: "taboo",
+      code: "07003",
+      name: "Taboo",
+      abilities: [
+        {
+          id: "reaction",
+          type: "reaction",
+          limitPer: "round",
+        },
+      ],
+    },
+  ],
+  skins: [
+    {
+      id: "07003_plan-of-action",
+      name: "Plan of Action",
+      image: {
+        width: 3448,
+        height: 3625,
+        face: {
+          width: 187,
+          height: 261,
+          left: 1745,
+          top: 1479,
+        },
+      },
+    },
+  ],
 };
