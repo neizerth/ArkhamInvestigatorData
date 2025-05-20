18dbce5e8701393f6119c846464e953b463a6981
Vladimir Yazykov <neizerth@gmail.com>
2025-05-20 22:40:03 +0300

    fix name

--- src/App.ts
+++ src/App.ts
@@ -1,29 +1,29 @@
 import { createImageCache, createInvestigatorsCache } from "./tasks";
-import { createDataCache } from "./tasks/createSignatureCache";
+import { createDataCache } from "./tasks/createDataCache";
 import { loadMetadata } from "./tasks/loadMetadata";
 import { prepareDirectories } from "./tasks/prepareDirectories";
 
 export class App {
-	async run(command: string) {
-		await prepareDirectories();
+  async run(command: string) {
+    await prepareDirectories();
 
-		switch (command) {
-			case "meta":
-				return await loadMetadata();
-			case "images":
-				return await createImageCache();
-			case "cache":
-				return await createInvestigatorsCache();
-			case "data":
-				return await createDataCache();
-			default:
-				return await this.start();
-		}
-	}
-	async start() {
-		await loadMetadata();
-		await createImageCache();
-		const languages = await createDataCache();
-		await createInvestigatorsCache(languages);
-	}
+    switch (command) {
+      case "meta":
+        return await loadMetadata();
+      case "images":
+        return await createImageCache();
+      case "cache":
+        return await createInvestigatorsCache();
+      case "data":
+        return await createDataCache();
+      default:
+        return await this.start();
+    }
+  }
+  async start() {
+    await loadMetadata();
+    await createImageCache();
+    const languages = await createDataCache();
+    await createInvestigatorsCache(languages);
+  }
 }
