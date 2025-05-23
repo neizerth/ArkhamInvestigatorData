79b679e81ae4f5e22f1aaccfa954910f313b3a94
Vladimir Yazykov <neizerth@gmail.com>
2025-05-23 17:25:49 +0300

    feat: updated mateo

--- src/data/signatures/campaigns/tfa/FatherMateo.ts
+++ src/data/signatures/campaigns/tfa/FatherMateo.ts
@@ -1,36 +1,36 @@
 import type { Investigator } from "@/model";
 
 export const FatherMateo: Investigator = {
-	code: "04004",
-	image: {
-		width: 2311,
-		height: 3000,
-		face: {
-			width: 394,
-			height: 445,
-			left: 912,
-			top: 1405,
-		},
-	},
-	variants: [
-		{
-			code: "90081",
-			type: "parallel",
-			name: "Aura of Faith",
-			abilities: [
-				{
-					id: "fast",
-					type: "fast",
-					limitPer: "round",
-				},
-			],
-		},
-	],
-	abilities: [
-		{
-			id: "reaction",
-			type: "reaction",
-			limitPer: "game",
-		},
-	],
+  code: "04004",
+  image: {
+    width: 2311,
+    height: 3000,
+    face: {
+      width: 394,
+      height: 445,
+      left: 912 + 200,
+      top: 1405,
+    },
+  },
+  variants: [
+    {
+      code: "90081",
+      type: "parallel",
+      name: "Aura of Faith",
+      abilities: [
+        {
+          id: "fast",
+          type: "fast",
+          limitPer: "round",
+        },
+      ],
+    },
+  ],
+  abilities: [
+    {
+      id: "reaction",
+      type: "reaction",
+      limitPer: "game",
+    },
+  ],
 };
