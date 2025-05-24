c56915daead58c5087cd065f001e2eccc934a3d4
Vladimir Yazykov <neizerth@gmail.com>
2025-05-24 08:55:34 +0300

    feat: tony

--- src/data/signatures/campaigns/tde/TonyMorgan.ts
+++ src/data/signatures/campaigns/tde/TonyMorgan.ts
@@ -1,16 +1,32 @@
 import type { Investigator } from "@/model";
 
 export const TonyMorgan: Investigator = {
-	code: "06003",
-	additionalAction: true,
-	image: {
-		width: 2320,
-		height: 2403,
-		face: {
-			width: 338,
-			height: 328,
-			left: 738 + 300,
-			top: 1145,
-		},
-	},
+  code: "06003",
+  additionalAction: true,
+  image: {
+    width: 2320,
+    height: 2403,
+    face: {
+      width: 338,
+      height: 328,
+      left: 738 + 300,
+      top: 1145,
+    },
+  },
+  skins: [
+    {
+      id: "06003_colt",
+      name: ".38 Long Colt",
+      image: {
+        width: 1745,
+        height: 2109,
+        face: {
+          width: 204,
+          height: 215,
+          left: 858 - 200,
+          top: 879,
+        },
+      },
+    },
+  ],
 };
