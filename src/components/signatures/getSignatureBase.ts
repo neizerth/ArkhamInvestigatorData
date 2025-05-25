65522305972f08dcdf804dc72879642e58f13a26
Vladimir Yazykov <neizerth@gmail.com>
2025-05-25 15:24:55 +0300

    fix: custom investigators category

--- src/components/signatures/getSignatureBase.ts
+++ src/components/signatures/getSignatureBase.ts
@@ -3,33 +3,34 @@ import { omit } from "ramda";
 import { getCardSpecial } from "./special";
 
 export const getSignatureBase = (card: ArkhamCardsInvestigator) => {
-	const base = omit(
-		[
-			"real_name",
-			"real_subname",
-			"real_text",
-			"real_traits",
-			"real_flavor",
-			"translations",
-			"real_taboo_original_text",
-			"real_taboo_text_change",
-			"pack",
-		],
-		card,
-	);
+  const base = omit(
+    [
+      "real_name",
+      "real_subname",
+      "real_text",
+      "real_traits",
+      "real_flavor",
+      "translations",
+      "real_taboo_original_text",
+      "real_taboo_text_change",
+      "pack",
+    ],
+    card
+  );
 
-	const special = getCardSpecial(card);
+  const special = getCardSpecial(card);
 
-	if (!special) {
-		return;
-	}
-	const canonical = card.pack.official;
-	const official = card.pack.cycle.code === "zffg" || canonical;
-	return {
-		...base,
-		...special,
-		taboo: card.taboo_set?.id !== 0 && card.taboo_set !== null,
-		canonical,
-		official,
-	};
+  if (!special) {
+    return;
+  }
+  const canonical = card.pack.official;
+  const official =
+    (card.pack.code !== "zcu" && card.pack.cycle.code === "zffg") || canonical;
+  return {
+    ...base,
+    ...special,
+    taboo: card.taboo_set?.id !== 0 && card.taboo_set !== null,
+    canonical,
+    official,
+  };
 };
