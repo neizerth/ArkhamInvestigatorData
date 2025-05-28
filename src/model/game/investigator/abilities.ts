754e554d4f4abffc8fe08558834c98ec49f477be
Vladimir Yazykov <neizerth@gmail.com>
2025-05-28 09:41:17 +0300

    fix: changed field name

--- src/model/game/investigator/abilities.ts
+++ src/model/game/investigator/abilities.ts
@@ -23,7 +23,7 @@ export type InvestigatorAbilityType =
   | {
       type: "counter";
       icon?: string;
-      defaultValue?: number;
+      intitalValue?: number;
       min?: number;
       max?: number;
     };
