e8e1c74f581ac408615196889e878e0cebdb58b7
Vladimir Yazykov <neizerth@gmail.com>
2025-05-29 08:56:26 +0300

    feat: added zhm

--- src/data/signatures/custom/side/index.ts
+++ src/data/signatures/custom/side/index.ts
@@ -1,6 +1,7 @@
 import zcu from "./zcu";
 import zez from "./zez";
+import zhm from "./zhm";
 import zinv from "./zinv";
 import zjc from "./zjc";
 
-export default [...zez, ...zjc, ...zinv, ...zcu];
+export default [...zez, ...zjc, ...zinv, ...zcu, ...zhm];
