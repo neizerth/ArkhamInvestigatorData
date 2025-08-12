a0b5341601c38b55906b9e4bc533d5f06b2c1b4e
Vladimir Yazykov <neizerth@gmail.com>
2025-08-12 12:25:45 +0300

    fix: declension

--- src/data/i18n/declension.ts
+++ src/data/i18n/declension.ts
@@ -2,5 +2,8 @@ type NameDeclension = Record<string, string>;
 export const dativeNameDeclension: Record<string, NameDeclension> = {
 	ru: {
 		"04244": "Телу йитианина",
+		"04004": "Отцу Матео",
+		"90081": "Отцу Матео",
+		"10661": "Расколотому Я",
 	},
 };
