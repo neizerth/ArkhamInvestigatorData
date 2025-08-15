71b7b5bec768f6987912020ec3d5c81f41442c56
Vladimir Yazykov <neizerth@gmail.com>
2025-08-15 08:47:26 +0300

    fix: filenames

--- cli/compress/avif.sh
+++ cli/compress/avif.sh
@@ -4,4 +4,4 @@ set -e
 cd ./dist/images
 
 zip -r -9 avif.zip avif/
-zip -r -9 android.gte12.zip avif/ -x "*/grayscale/*"
+zip -r -9 avif.color.zip avif/ -x "*/grayscale/*"
--- cli/compress/webp.sh
+++ cli/compress/webp.sh
@@ -4,4 +4,4 @@ set -e
 cd ./dist/images
 
 zip -r -9 webp.zip webp/
-zip -r -9 android.lt12.zip webp/ -x "*/grayscale/*"
+zip -r -9 webp.color.zip webp/ -x "*/grayscale/*"
