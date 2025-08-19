4a752399fd1468a99f6f4c67aef6180fc36158c2
Vladimir Yazykov <neizerth@gmail.com>
2025-08-20 00:13:23 +0300

    refactor: jim base

--- src/components/chaos-bag/getSignatureTokenReference.ts
+++ src/components/chaos-bag/getSignatureTokenReference.ts
@@ -1,6 +1,3 @@
-import { InvestigatorCode } from "@/config/codes";
-import { propEq, reject } from "ramda";
-import { appendTokenEffect } from "./effects";
 import { getChaosBagTokenReference } from "./getChaosBagTokenReference";
 
 type Options = {
@@ -8,25 +5,8 @@ type Options = {
 	text: string;
 };
 
-export const getSignatureTokenReference = ({ code, text }: Options) => {
+export const getSignatureTokenReference = ({ text }: Options) => {
 	const reference = getChaosBagTokenReference([text]);
-	const lines = text.split("\n");
-
-	if (code === InvestigatorCode.JimCulver.base) {
-		const elderSignEffect = lines[1];
-
-		const data = appendTokenEffect({
-			items: [
-				{
-					token: "elderSign",
-					effect: elderSignEffect,
-				},
-			],
-			data: reference,
-		});
-
-		return reject(propEq("group", "type"), data);
-	}
 
 	return reference;
 };
