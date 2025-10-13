523a5abe51ad16a791ed8d7435c3dd2a115a99e7
Vladimir Yazykov <neizerth@gmail.com>
2025-10-13 23:10:32 +0300

    fix: zoey

--- src/data/signatures/campaigns/dwl/ZoeySamaras.ts
+++ src/data/signatures/campaigns/dwl/ZoeySamaras.ts
@@ -49,8 +49,8 @@ export const ZoeySamaras: Investigator = {
 				face: {
 					width: 459,
 					height: 472,
-					left: 1788 - 450,
-					top: 1400 + 500,
+					left: 1788 - 150,
+					top: 1400 + 100,
 				},
 			},
 		},
