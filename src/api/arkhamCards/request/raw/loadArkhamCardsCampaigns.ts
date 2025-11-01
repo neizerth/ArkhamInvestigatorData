5f38b9430aa33fe90d018f90fef6f387fc0dcb2a
Vladimir Yazykov <neizerth@gmail.com>
2025-11-01 12:00:59 +0300

    fix: ac json urls

--- src/api/arkhamCards/request/raw/loadArkhamCardsCampaigns.ts
+++ src/api/arkhamCards/request/raw/loadArkhamCardsCampaigns.ts
@@ -3,7 +3,7 @@ import type { ArkhamCardsCampaign } from "../../model";
 
 export const loadArkhamCardsCampaigns = async () => {
 	console.log("loading Arkham Cards Campaigns");
-	const url = `${baseUri}/assets/generated/allCampaigns.json`;
+	const url = `${baseUri}/assets/generated/all_campaigns.txt`;
 
 	const response = await fetch(url);
 	return (await response.json()) as ArkhamCardsCampaign[];
--- src/api/arkhamCards/request/raw/odds/loadArkhamCardsChaosOdds.ts
+++ src/api/arkhamCards/request/raw/odds/loadArkhamCardsChaosOdds.ts
@@ -4,8 +4,8 @@ import type { ArkhamCardsChaosOdd } from "../../../model";
 export const loadArkhamCardsChaosOdds = async (language = "en") => {
 	console.log(`loading Arkham Cards Chaos Odds for language: ${language}`);
 
-	const locale = language === "en" ? "" : `_${language}`;
-	const url = `${baseUri}/assets/generated/chaosOdds${locale}.json`;
+	const locale = language === "en" ? "" : `_${language.replace("-", "_")}`;
+	const url = `${baseUri}/assets/generated/chaos_odds${locale}.txt`;
 
 	const response = await fetch(url);
 	return (await response.json()) as ArkhamCardsChaosOdd[];
