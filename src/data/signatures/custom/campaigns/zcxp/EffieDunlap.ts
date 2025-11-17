9e55a2117187adf72179b01843623a46fd13122a
Vladimir Yazykov <neizerth@gmail.com>
2025-11-17 22:33:35 +0900

    fix: image version

--- src/data/signatures/custom/campaigns/zcxp/EffieDunlap.ts
+++ src/data/signatures/custom/campaigns/zcxp/EffieDunlap.ts
@@ -3,6 +3,7 @@ import type { Investigator } from "@/model";
 export const EffieDunlap: Investigator = {
 	code: "74774218-0085-41de-bf49-bded1325e0db",
 	image: {
+		version: 2,
 		width: 3345,
 		height: 3590,
 		face: {
