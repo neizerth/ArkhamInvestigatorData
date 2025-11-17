8496c89a31d6d2f733af431d346fd6b4b390e34c
Vladimir Yazykov <neizerth@gmail.com>
2025-11-17 22:48:58 +0900

    fix

--- src/api/arkham.build/config/cycles.ts
+++ src/api/arkham.build/config/cycles.ts
@@ -1,2 +1,5 @@
 export const ignoreCyclesIds = ["alice_in_wonderland", "ordinary_citizens"];
-export const supportedCyclesIds = ["circus_ex_mortis_investigator_expansion"];
+export const supportedCyclesIds = [
+	// "circus_ex_mortis_investigator_expansion"
+	"none",
+];
