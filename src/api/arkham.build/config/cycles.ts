f3f10028880171920e06d18dcc2f6f6a08afa5f1
Vladimir Yazykov <neizerth@gmail.com>
2026-02-16 09:50:25 +0100

    fix: arkham.build omit zgoo

--- src/api/arkham.build/config/cycles.ts
+++ src/api/arkham.build/config/cycles.ts
@@ -2,6 +2,7 @@ export const ignoreCyclesIds = [
 	"alice_in_wonderland",
 	"ordinary_citizens",
 	"circus_ex_mortis_investigator_expansion",
+	"ghosts_of_onigawa_investigator_expansion",
 ];
 export const supportedCyclesIds = [
 	// "circus_ex_mortis_investigator_expansion",
